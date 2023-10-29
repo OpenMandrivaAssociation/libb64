9ac4cf1edc279b9749ef368967a7ee9a552f62c9
mandian <mandian@users.noreply.github.com>
2023-10-29 15:35:52 +0000

    add check-update.sh

---
+++ check-update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -x
+package=libb64
+curl -s -L -A 'Mozilla/5.0 (X11; Linux x86_64)' https://sourceforge.net/projects/${package}/files/ |grep -b1 'Download Latest Version' |tail -n1 |sed -e "s,.*>${package}-,,;s,\.zip.*,,"
+
